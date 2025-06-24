2e75ea5451150cc68bce212662d00b90e9d1db17
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-06-24 22:50:38 +0530

    added somechanges

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,10 +1,10 @@
 import React,{useState} from 'react'
-import assets from '../assets/assets'
+import assets, { userDummyData } from '../assets/assets'
 import {useNavigate} from 'react-router-dom'
 const Sidebar = ({selectedUser, setSelectedUser}) => {
   const navigate = useNavigate()
   return (
-    <div> className={`bg-[#8185b2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser}`}
+    <div className={`bg-[#8185b2]/10 h-full p-5 rounded-r-xl overflow-y-scroll text-white ${selectedUser?"max-md:hidden":""}`}>
       <div className='pb-5'>
         <div className='flex justify-between items-center'>
           <img src={assets.logo} alt="logo" className='max-w-40'/>
@@ -17,6 +17,20 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
             </div>
           </div>
         </div>
+        <div className='bg-[#282142] rounded-full flex items-center gap-5 py-3 px-4 mt-5'>
+          <img src={assets.search_icon} alt="search" className='w-5'/>
+          < input type="text" className='bg-transparent border-none outline-none text-white text-xs placeholder=[@c8c8c8] flex-1' placeholder='SearchUser...'/>
+        </div>
+      </div>
+      <div className='flex flex-col'>
+        {userDummyData.map((user,index)=>(
+          <div>
+            <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-[35px] aspect-ratio-[1/1] rounded-full' />
+            <div className='flex flex-col leading-5'>
+
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   )
