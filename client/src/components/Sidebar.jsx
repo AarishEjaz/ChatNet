a3db03c61c56ad949adacc277ffcc3f0c26f3f5f
Aarish Ejaz <75199171+AarishEjaz@users.noreply.github.com>
2025-06-26 19:41:07 +0530

    made sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,6 +1,9 @@
 import React,{useState} from 'react'
 import assets, { userDummyData } from '../assets/assets'
 import {useNavigate} from 'react-router-dom'
+
+
+
 const Sidebar = ({selectedUser, setSelectedUser}) => {
   const navigate = useNavigate()
   return (
@@ -24,11 +27,17 @@ const Sidebar = ({selectedUser, setSelectedUser}) => {
       </div>
       <div className='flex flex-col'>
         {userDummyData.map((user,index)=>(
-          <div>
+          <div onClick={()=>{setSelectedUser(user)}} key={index} className={`relative flex items-center gap-2 p-2 pl-4 rounded cursor-pointer max-sm:text-sm ${selectedUser?._id==user._id && 'bg-[#282143]/50'} `}>
             <img src={user?.profilePic || assets.avatar_icon} alt="" className='w-[35px] aspect-ratio-[1/1] rounded-full' />
             <div className='flex flex-col leading-5'>
-
+              <p>{user.fullName}</p>
+              {
+                index<3
+                ? <span className='text-green-400 text-xs'>Online</span>
+                :<span className='text-neutral-400 text-xs'>Offline</span>
+              }
             </div>
+            {index>2&&<p className='absolute top-4 right-4 text-xs h-5 w-5 flex justfy-center items-center rounded-full bg-violet-500/50'>{index}</p>}
           </div>
         ))}
       </div>
